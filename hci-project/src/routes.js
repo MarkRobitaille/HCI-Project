b43d177bac73cb1dc41c9d987c6d5697acf6d06d
Mark Robitaille <mark.rj.robitaille@gmail.com>
2019-11-12 12:37:55 -0600

    Added some more routes, formatted code.

--- hci-project/src/routes.js
+++ hci-project/src/routes.js
@@ -4,11 +4,43 @@ const routes = [
     {
         path: '/',
         component: HelloWorld,
-        name: "Hello World",
+        name: "Home",
         meta: {
-            title: "TEST"
+            title: "Home"
         }
-    }
+    },
+    {
+        path: '/messenger',
+        component: HelloWorld,
+        name: "Messenger",
+        meta: {
+            title: "Messenger"
+        }
+    },
+    {
+        path: '/calendar',
+        component: HelloWorld,
+        name: "Calendar",
+        meta: {
+            title: "Calendar"
+        }
+    },
+    {
+        path: '/lists',
+        component: HelloWorld,
+        name: "Lists",
+        meta: {
+            title: "Lists"
+        }
+    },
+    {
+        path: '/settings',
+        component: HelloWorld,
+        name: "Settings",
+        meta: {
+            title: "Settings"
+        }
+    },
 ]
 
 export default routes;
