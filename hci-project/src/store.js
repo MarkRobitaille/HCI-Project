12cdb75577d3b996f89d76163d9fa26cd41a12c3
Mark Robitaille <mark.rj.robitaille@gmail.com>
2019-11-13 14:53:33 -0600

    Started progress towards reading in test data

--- hci-project/src/store.js
+++ hci-project/src/store.js
@@ -1,26 +1,47 @@
 const storeModule = {
     state: {
-        counter: 1
+        counter: 1,
+        users: [],
+        messenger: [],
+        calendar: [],
+        lists: []
     },
 
     getters: {
         getCounter: function (state) {
             return state.counter;
+        },
+        getUsers: function (state) {
+            return state.users;
+        },
+        getMessenger: function (state) {
+            return state.messenger;
+        },
+        getCalendar: function (state) {
+            return state.calendar;
+        },
+        getList: function (state) {
+            return state.lists;
         }
     },
 
     mutations: {
         incrementCounter(state) {
-            state.counter++
-        }
-        
+            state.counter++;
+        },
+        // setUsers(state, payload) {
+
+        // }
     },
 
     actions: {
         incrementCounter({ commit }) {
             commit('incrementCounter')
-        }
-    },
+        },
+        // initializeJSONData({commit}) {
+        //     // commit()
+        // }
+    }
 
 }
 
