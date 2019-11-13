0d06bb546144e519cd007b20d597d8d41bd030d2
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2019-11-13 14:44:21 -0600

    Created list sidebar and content

--- hci-project/src/components/Lists.vue
+++ hci-project/src/components/Lists.vue
@@ -2,6 +2,17 @@
   <div> <!-- Can only have 1 parent element in a component -->
     <!-- All template inside of here -->
     <h1>Lists</h1>
+    <div class="listBar">
+      <a href="#/lists" class="barItem">Grocery List</a>
+    </div>
+
+    <div class="listContent">
+      <ul id="GroceryList" class="list">
+        <li class="listItem">Pizza</li>
+        <li class="listItem">Cheese</li>
+        <li class="listItem">Milk</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -36,5 +47,36 @@ export default {
 /* Use classes to create css */
 /* If you want dynamic application of css, create a computed variable in computed section */
 /* Then apply by binding it to html tag by using :class="computedClass" */
+/* Include the padding and border in an element's total width and height */
+.list{
+  text-align: left;
+  padding-left: 5%;
+  padding-top: 1%;
+  margin: 0;
+  list-style: none;
+}
+.listItem{
+  padding: 1%;
+}
+.listBar {
+  background-color: blueviolet;
+  float: left;
+  position: fixed;
+  width: 25%;
+  height: 100%;
+  padding: 1%;
+  margin-top: 0;
+}
+.barItem{
+  width: 25%;
+  text-align: center;
+}
 
+.listContent{
+  background-color: lightpink;
+  width: 75%;
+  height: 100%;
+  margin-left: 25%;
+  position: fixed;
+}
 </style>
