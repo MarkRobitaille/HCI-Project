0845d50c72259ca8f88531ee7030a019a6684fa9
Ceilidh Simon <43501977+LeeSimon77@users.noreply.github.com>
2019-11-14 14:43:15 -0600

    Added buttons to the list

--- hci-project/src/components/Lists.vue
+++ hci-project/src/components/Lists.vue
@@ -1,17 +1,47 @@
 <template>
   <div> <!-- Can only have 1 parent element in a component -->
     <!-- All template inside of here -->
-    <h1>Lists</h1>
-    <div class="listBar">
+    <div class="header">
+      <h1>Lists</h1>
+    </div>
+
+    <!-- <div class="listBar">
       <a href="#/lists" class="barItem">Grocery List</a>
+      <button class="addListButton">Add List</button>
+    </div> -->
+
+     <div class="message_list">
+      <div class="message_list_items">
+        <!-- <div
+          v-for="(item, i ) in messenger"
+          :key="i"
+          :class="{'convo': true, 'activeConvo': i === selected}"
+          @click="changeSelected(i)"
+        >
+          <div v-if="item.notification" class="notification"></div>
+          <img :src="require('../assets/' + item.image)" />
+          <p class="convoHeader">{{item.name}}</p>
+          <p v-if="item.messages.length>0">"{{shortenString(item.messages[item.messages.length-1].message)}}"</p>
+        </div> -->
+        <button class="addListButton">Add List</button>
+        <p class="convoHeader">Grocery List</p>
+      </div>
     </div>
 
     <div class="listContent">
+      <h2>Grocery List </h2>
+      <button class="addItemButton">Add Item</button>
+      
       <ul id="GroceryList" class="list">
-        <li class="listItem">Pizza</li>
-        <li class="listItem">Cheese</li>
-        <li class="listItem">Milk</li>
+        <li class="listItem"><input id="pizzaItem" type="text" value="Pizza" class="listInput">
+          <button class="deleteItemButton">Remove</button></li>
+        <li class="listItem"><input type="text" value="Cheese" class="listInput">
+          <button class="deleteItemButton">Remove</button></li>
+        <li class="listItem"><input type="text" value="Milk" class="listInput">
+          <button class="deleteItemButton">Remove</button></li>
       </ul>
+
+      <button class="deleteListButton">Delete List</button>
     </div>
   </div>
 </template>
@@ -36,7 +66,6 @@ export default {
 
   },
   methods: { // Methods in this component
-
   }
 }
 </script>
@@ -48,35 +77,103 @@ export default {
 /* If you want dynamic application of css, create a computed variable in computed section */
 /* Then apply by binding it to html tag by using :class="computedClass" */
 /* Include the padding and border in an element's total width and height */
+.header {
+  background: white;
+  border-bottom: solid 1px #ededed;
+  height: 10vh;
+  color: black;
+  padding-bottom: 2.5vh;
+}
+
 .list{
   text-align: left;
-  padding-left: 5%;
-  padding-top: 1%;
-  margin: 0;
+  padding: 0;
+  width: 100%;
+  margin-left: 5vw;
+  margin-top: 10vh;
   list-style: none;
 }
 .listItem{
-  padding: 1%;
+  padding-top: 1%;
+  padding-bottom: 1%;
+  width: 80%;
+  margin: 0;
 }
 .listBar {
-  background-color: blueviolet;
-  float: left;
-  position: fixed;
+  padding-top: 5vh;
+  background: blueviolet;
   width: 25%;
-  height: 100%;
-  padding: 1%;
-  margin-top: 0;
+  height: 87.5vh;
+  float: left;
+  overflow-y: scroll;
 }
 .barItem{
-  width: 25%;
+  width: 25vw;
   text-align: center;
 }
 
 .listContent{
   background-color: lightpink;
   width: 75%;
-  height: 100%;
-  margin-left: 25%;
-  position: fixed;
+  height: 87.5vh;
+  overflow-y: scroll;
+}
+.listInput{
+  width: 45vw;
+  background: none;
+  border: none;
+}
+.deleteItemButton{
+  right: 3vw;
+  position: absolute;
+  min-height: 10px;
+  height: 3vh;
+}
+
+.addItemButton{
+  margin-top: 1vh;
+  right: 3vw;
+  position: absolute;
+  min-height: 10px;
+  height: 5vh;
+}
+.addListButton{
+  margin-top: 1vh;
+  min-height: 10px;
+  height: 5vh;
+}
+
+.convoHeader {
+  font-weight: bold;
+}
+
+.message_list {
+  background: #def5fc;
+  width: 25%;
+  height: 87.5vh;
+  float: left;
+}
+
+.message_list_items {
+  width: 100%;
+  height: 80vh;
+  overflow-y: scroll;
+}
+
+.notification {
+   background:red;
+   position:absolute;
+   border-radius:50%;
+   width: 2.5vh;
+   height: 2.5vh;
+   margin-left: 1vw;
+}
+.deleteListButton{
+  bottom: 5vh;
+  right: 3vw;
+  position: absolute;
+  min-height: 10px;
+  height: 5vh;
 }
 </style>
+
