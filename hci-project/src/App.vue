b78a3da74f90fd25b27736cf0d86cc15cbace20d
Mark Robitaille <mark.rj.robitaille@gmail.com>
2019-11-16 15:40:35 -0600

    Enabled div navigation on navbar

--- hci-project/src/App.vue
+++ hci-project/src/App.vue
@@ -1,40 +1,48 @@
 <template>
   <div class="main">
     <div class="navbar">
-
-      <div type="button" class="navTabs" id="homeTab">
-        <!-- <font-awesome-icon icon="home" size="4x"/> -->
-        <a href="#/" :class="activeSection==0? 'activeNavbarSection' : 'navbarSection'"> HOME</a>
+      <div
+        class="navTabs"
+        id="homeTab"
+        :class="activeSection==0? 'activeNavbarSection' : 'navbarSection'"
+        @click="changeSection('/')"
+      >
+        <!-- <font-awesome-icon icon="home" size="4x"/> -->HOME
       </div>
 
-      <div class="navTabs" id = "messengerTab">
-        <a href="#/messenger" :class="activeSection==1? 'activeNavbarSection' : 'navbarSection'">
-          <div v-if="unreadMessageCount>0" class="notification"></div>Messenger
-        </a>
+      <div
+        class="navTabs"
+        id="messengerTab"
+        :class="activeSection==1? 'activeNavbarSection' : 'navbarSection'"
+        @click="changeSection('/messenger')"
+      >
+        <div v-if="unreadMessageCount>0" class="notification"></div>Messenger
       </div>
 
-      <div class="navTabs" id="calendarTab">
-        <a
-          href="#/calendar"
-          :class="activeSection==2? 'activeNavbarSection' : 'navbarSection'"
-        >Calendar</a>
-      </div>
+      <div
+        class="navTabs"
+        id="calendarTab"
+        :class="activeSection==2? 'activeNavbarSection' : 'navbarSection'"
+        @click="changeSection('/calendar')"
+      >Calendar</div>
 
-      <div class="navTabs" id="listTab">
-        <a href="#/lists" :class="activeSection==3? 'activeNavbarSection' : 'navbarSection'">Lists</a>
-      </div>
-      
-      <div class="navTabs" id="settingsTab">
-        <a
-          href="#/settings"
-          :class="activeSection==4? 'activeNavbarSection' : 'navbarSection'"
-        >Settings</a>
-      </div>
+      <div
+        class="navTabs"
+        id="listTab"
+        :class="activeSection==3? 'activeNavbarSection' : 'navbarSection'"
+        @click="changeSection('/lists')"
+      >Lists</div>
+
+      <div
+        class="navTabs"
+        id="settingsTab"
+        :class="activeSection==4? 'activeNavbarSection' : 'navbarSection'"
+        @click="changeSection('/settings')"
+      >Settings</div>
 
       <div class="logoutButton">
         <button>Log out</button>
       </div>
-
     </div>
     <div class="content">
       <div id="app">
@@ -59,13 +67,16 @@ export default {
       activeSection: 0,
       tempDate: null,
       currDate: null,
-      convertedDate: null,
+      convertedDate: null
     };
   },
   created() {
     this.$store.dispatch("initializeJSONData");
     let today = new Date();
-    this.$store.dispatch("setToday", { month: today.getMonth(), day: today.getDate() - 1})
+    this.$store.dispatch("setToday", {
+      month: today.getMonth(),
+      day: today.getDate() - 1
+    });
     this.setActiveSection(this.$route.name);
   },
   watch: {
@@ -81,6 +92,11 @@ export default {
     })
   },
   methods: {
+    changeSection(path) {
+      if (this.$route.path !== path) {
+        this.$router.push(path);
+      }
+    },
     setActiveSection(routeName) {
       switch (routeName) {
         case "Home":
@@ -106,7 +122,7 @@ export default {
 
 <style>
 /* IDs */
-@import url('https://fonts.googleapis.com/css?family=Alata|Roboto&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Alata|Roboto&display=swap");
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -148,7 +164,7 @@ h1 {
   margin: 0;
 }
 .navbar a {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   text-decoration: none;
   color: white;
   font-size: 2vw;
@@ -181,58 +197,86 @@ h1 {
   height: 1.25vw;
 }
 
-.navTabs{
-
+.navTabs {
   background-color: black;
   margin-left: 4vh;
   height: 15vh;
   border-top-left-radius: 25px;
   border-bottom-left-radius: 25px;
   /* border-bottom: solid 1.5px white; */
-
 }
 
-#homeTab{
-
+#homeTab {
   /* margin-top: 0.5vh; */
-  background: #7F7FD5;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+  background: #7f7fd5; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #91eae4,
+    #86a8e7,
+    #7f7fd5
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #91eae4,
+    #86a8e7,
+    #7f7fd5
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
-#messengerTab{
-
-  background: #43C6AC;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #F8FFAE, #43C6AC);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #F8FFAE, #43C6AC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+#messengerTab {
+  background: #43c6ac; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #f8ffae,
+    #43c6ac
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #f8ffae,
+    #43c6ac
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
-#calendarTab{
-
-  background: #f12711;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #f5af19, #f12711);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #f5af19, #f12711); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+#calendarTab {
+  background: #f12711; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #f5af19,
+    #f12711
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #f5af19,
+    #f12711
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
-#listTab{
-
-  background: #9796f0;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #fbc7d4, #9796f0);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #fbc7d4, #9796f0); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-
+#listTab {
+  background: #9796f0; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #fbc7d4,
+    #9796f0
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #fbc7d4,
+    #9796f0
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
-#settingsTab{
-
-  background: #EECDA3;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to right, #EF629F, #EECDA3);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to right, #EF629F, #EECDA3); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-
+#settingsTab {
+  background: #eecda3; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #ef629f,
+    #eecda3
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #ef629f,
+    #eecda3
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
 .leftIconOffset {
